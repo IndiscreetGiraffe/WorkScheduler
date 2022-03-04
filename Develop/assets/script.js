92ce687c1a6ec9b441eb92e3e37f155e7d8d88e1
Kyleah Justice <kikij432@gmail.com>
2022-03-04 04:20:05 -0700

    using the moment docs, i got the current date to show on the main page

---
+++ Develop/assets/script.js
@@ -0,0 +1,7 @@
+var currentDay = document.getElementById("currentDay");
+var timeBlock = document.getElementById("time-block");
+var hour = document.getElementById("hour");
+
+
+currentDay.innerText = moment().format("dddd MMM Do YYYY");
+
