bdcc1778a9f22eb0027f09e6be70f44a9dda8542
Kyleah Justice <kikij432@gmail.com>
2022-03-04 05:04:38 -0700

    i got the save button to click in the console and show in local storage. going to chagne up the function to show the past present and future. Going to trt some if statements

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,17 +1,25 @@
 var currentDay = document.getElementById("currentDay");
 var timeBlock = document.getElementById("time-block");
 var hour = document.getElementById("hour");
-
+var saveBtn = $(".saveBtn");
 
 currentDay.innerText = moment().format("dddd MMM Do YYYY");
 
 var currentHour = moment().format('h');
 var currentTime = parseInt(currentHour);
 
-if (currentHour > currentTime) {
-    $('.input').addClass('future');
-} else if (currentTime == 9) {
-    $('.input').addClass('present');
-} else {
-    $('.input').addClass('past');
-};
+//if (currentHour > currentTime) {
+    //$('.input').addClass('future');
+//} else if (currentTime == 9) {
+    //$('.input').addClass('present');
+//} else {
+    //$('.input').addClass('past');
+//};
+
+saveBtn.on("click", function() {
+    console.log(this);
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".input").val();
+
+    localStorage.setItem(time, plan);
+});
