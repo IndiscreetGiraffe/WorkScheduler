4de75fe10b8ecfd745808c1b961d7b7ff9cec58f
Kyleah Justice <kikij432@gmail.com>
2022-03-04 04:48:06 -0700

    I got the styling to work and I think i got the past present and future thing to work. Its grey right now. but it took a lot of reading and searching to find a good way to do it

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -5,3 +5,13 @@ var hour = document.getElementById("hour");
 
 currentDay.innerText = moment().format("dddd MMM Do YYYY");
 
+var currentHour = moment().format('h');
+var currentTime = parseInt(currentHour);
+
+if (currentHour > currentTime) {
+    $('.input').addClass('future');
+} else if (currentTime == 9) {
+    $('.input').addClass('present');
+} else {
+    $('.input').addClass('past');
+};
